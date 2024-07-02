404d045c5562fe832e781f9b71ee285a294b3030
thai09123 <109845812+thai09123@users.noreply.github.com>
2024-07-02 19:47:44 +0700

    Add files via upload
    creat crad

---
+++ config.js
@@ -0,0 +1,13 @@
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'CỐC CỐC CỐC';
+const nameGirl = 'Cua oiiii';
+const titleCard = 'Lời anh mún nói';
+const contentCard = 'Đây là những lời mà anh mún nói với em nhưng mà anh vẫn chưa có dịp để nói, nhưng mà nhân ngày hôm nay là ngày sinh nhật của em đầu tiên anh chúc em sinh nhật vui vẻ và lun xink đẹp lun,thì anh muốn nói là từ cái ngày mà em bảo với anh là mún nghe anh nói những cái ưu phiền ra là lúc đó tui cảm thấy đây là người mà anh nên yêu thương nhiều hơn, anh mong em cho anh cơ hội để được trở thành 1 nữa còn lại của em để quan tâm và chăm sóc em.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh-em.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
